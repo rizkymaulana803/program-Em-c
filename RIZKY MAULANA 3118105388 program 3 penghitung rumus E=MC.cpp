bd5a314385b4918b21d29d32c461bf93f5a01a91
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-03 18:46:52 +0700

    Add files via upload

---
+++ RIZKY MAULANA 3118105388 program 3 penghitung rumus E=MC.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int C,M, E;
+	
+	cout<<"masukan C:";
+	cin>>C;
+	cout<<"masukan M:";
+	cin>>M;
+	E=(C * M)*(C * M);
+	cout<<"hasil kuadrat CMadalah"<<E;
+	return 0;
+}
